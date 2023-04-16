ab9c4e5377302fb306a8fd7a9ffe1f6bf4d21392
mac <ruslan@scaleflex.com>
2023-04-16 22:10:16 +0700

    most simple login form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-function App() {
-  return (
-    <div className="App">
+import { useState } from "react";
 
+function App() 
+{
+  const [loginSuccessful, setLoginSuccessful] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { username, password } = document.forms[0];
+
+    if (username.value === 'user1' && password.value === 'pass1') 
+    { // Mimic backend check
+      setLoginSuccessful(true);
+    }
+  };
+
+  const renderForm = (
+
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username </label>
+          <input type="text" name="username" required />
+        </div>
+        <div>
+          <label>Password </label>
+          <input type="password" name="password" required />
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+
+  );
+
+  return (
+    <div>
+      <div>
+        <div>Sign In</div>
+        {loginSuccessful ? <div>User is successfully logged in</div> : renderForm}
+      </div>
     </div>
   );
 }
